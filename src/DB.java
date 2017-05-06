c3c5dc8c56a9fe5e5b1764da145faae37cb670b4
jubayed131 <jubayed.cste@gmail.com>
2017-05-06 14:07:37 +0200

    Pay your attention

---
+++ src/DB.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+
+
+public class DB {
+    static Connection conn=null;
+    public static Connection java_db(){
+    try{
+        Class.forName("org.sqlite.JDBC");
+        Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Jubayed\\Documents\\NetBeansProjects\\NokshaEmployeePayroll\\nokshadatabase.sqlite");
+        return conn;
+    }
+    catch(Exception e){
+    JOptionPane.showMessageDialog(null, e);
+    return null;
+    }
+    }
+}
